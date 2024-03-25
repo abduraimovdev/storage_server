376610d15574dda46b608c10e67886410b30d324
abduraimov <kenzo778118@gmail.com>
2024-03-25 09:36:35 +0500

    Update

---
+++ bin/handler/main_handler.dart
@@ -0,0 +1,16 @@
+part of '../router/router.dart';
+
+Future<Response> _mainHandler(Request req) async {
+  final String body = await req.readAsString();
+  await infoLogWrite(req, body);
+  try {
+    final sqlReq = jsonDecode(body) as Map;
+    db.execute(sqlReq["sql"]);
+    return Response.ok("ok");
+  } catch (e, s) {
+    print("Error : $e");
+    print("Stack : $s");
+    errorLogWrite(e, s);
+    return Response.badRequest();
+  }
+}
--- bin/router/router.dart
+++ bin/router/router.dart
@@ -10,10 +10,11 @@ part '../handler/create_table_handler.dart';
 part '../handler/delete_handler.dart';
 part '../handler/insert_handler.dart';
 part '../handler/select_handler.dart';
+part '../handler/main_handler.dart';
 
 final router = Router()
   ..get('/select/', _selectHandler)
   ..post('/insert/', _insertHandler)
   ..delete('/delete_handler.dart/', _deleteHandler)
-  ..post('/create-table/', _createTableHandler);
-
+  ..post('/create-table/', _createTableHandler)
+  ..post('/main/', _mainHandler);
